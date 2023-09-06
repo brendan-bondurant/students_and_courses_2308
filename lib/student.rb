f92a98acd42cc67b629787b2b2b0f858fcb3571b
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-09-06 09:32:27 -0600

    Complete iteration_1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
 class Student
+  attr_reader :name, :age, :scores
+  def initialize (student_info)
+    @name = student_info.fetch(:name)
+    @age = student_info.fetch(:age)
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    total = []
+    scores.each do |score|
+      total << score.to_f
+    end
+    total.sum / scores.count
+  end
+  
   
 end
